a30408f83c39b64bb6fc012a2539fdc08b2a9311
thompson-jacob <63691359+thompson-jacob@users.noreply.github.com>
2023-12-05 14:33:03 -0700

    created thank you letters

--- lib/event_manager.rb
+++ lib/event_manager.rb
@@ -1,13 +1,41 @@
 require 'csv'
+require 'erb'
 require 'google/apis/civicinfo_v2'
 
-civic_info = Google::Apis::CivicinfoV2::CivicInfoService.new
-civic_info.key = 'AIzaSyClRzDqDh5MsXwnCWi0kOiiBivP6JsSyBw'
 
-response = civic_info.representative_info_by_address(address: 20010, levels: 'country', roles: ['legislatorUpperBody', 'legislatorLowerBody'])
-pp response.officials
+
+def clean_zipcode(zip)
+  zip.to_s.rjust(5,'0')[0...5]
+end 
+
+def legislators_by_zipcode(zip)
+  civic_info = Google::Apis::CivicinfoV2::CivicInfoService.new
+  civic_info.key = 'AIzaSyClRzDqDh5MsXwnCWi0kOiiBivP6JsSyBw'
+
+  begin
+    legislators = civic_info.representative_info_by_address(
+      address: zip, 
+      levels: 'country',
+      roles: ['legislatorUpperBody', 'legislatorLowerBody']).officials
+  rescue
+    'You can find your representatives by visiting www.commoncause.org/take-action/find-elected-officials'
+    end
+end
+
+def save_thank_you_letter(id, form_letter)
+  Dir.mkdir('output') unless Dir.exist?('output')
+
+  filename = "output/thanks_#{id}.html"
+
+  File.open(filename, 'w') do |file|
+    file.puts form_letter
+  end
+end
 
 puts "Event Manager Initialized!"
+
+template_letter = File.read('form_letter.erb')
+erb_template =  ERB.new(template_letter)
 
 contents = CSV.open(
   'event_attendees.csv',
@@ -15,25 +43,14 @@ contents = CSV.open(
   header_converters: :symbol,
 )
 
-# def clean_zipcode(zip)
-#   if zip.nil?
-#     zip = "00000"
-#   elsif zip.length > 5
-#     zip = zip[0...5]
-#   elsif zip.length < 5
-#     zip = zip.rjust(5,'0')
-#   else
-#     zip
-#   end
-# end 
-
-def clean_zipcode(zip)
-  zip.to_s.rjust(5,'0')[0...5]
-end 
-
-
 contents.each do |row|
+  id = row[0]
   f_name = row[:first_name]
+
   zip = clean_zipcode(row[:zipcode])
-  # puts "#{f_name} #{zip}"
+  legislators = legislators_by_zipcode(zip)
+  form_letter = erb_template.result(binding)
+
+  save_thank_you_letter(id, form_letter)
 end
+
